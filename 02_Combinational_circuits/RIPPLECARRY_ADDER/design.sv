6211a015128040c729250e3acdd5755007317176
vinayak <vinayaksharma17037@gmail.com>
2026-09-13 12:36:58 +0530

    Update design.sv

--- 02_Combinational_circuits/RIPPLECARRY_ADDER/design.sv
+++ 02_Combinational_circuits/RIPPLECARRY_ADDER/design.sv
@@ -13,7 +13,7 @@ module ripplecarry(A0,A1,A2,A3,B0,B1,B2,B3,S0,S1,S2,S3,C0,C1,C2,C3,C4);
   output C1, C2 ,C3,C4; //carry output 
   output S0,S1,S2,S3; //sum output 
   
-  adder m1(.a(A0) , .b(B0), .cin(C0) , .cout(C1) , .sum(S0) ); 
+  adder m1(.a(A0) , .b(B0), .cin(C0) , .cout(C1) , .sum(S0) );     //instantiating the lower module ie- one bit adder to the ripple carry adder
   adder m2(.a(A1) , .b(B1), .cin(C1) , .cout(C2) , .sum(S1) ); 
   adder m3(.a(A2) , .b(B2), .cin(C2) , .cout(C3) , .sum(S2) ); 
   adder m4(.a(A3) , .b(B3), .cin(C3) , .cout(C4) , .sum(S3) ); 
